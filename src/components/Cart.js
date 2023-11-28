0c1e3f91414da1cd875a38643449e93d7d35a40f
Rachit <rachitshrm96@gmail.com>
2023-11-28 23:39:52 +0530

    updated cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -6,6 +6,8 @@ const Cart =()=>{
 
     const dispatch=useDispatch()
     const cartItems = useSelector(store => store.cart.items)
+    const totalPrice = cartItems.reduce((total, item) => total + item.card.info.price, 0);
+
     const handleClearCart=()=>{
         dispatch(clearCart());
     }
@@ -16,8 +18,9 @@ const Cart =()=>{
             onClick={()=>handleClearCart()}>Clear Cart
         </button>
         {cartItems.map(item=>
-            <FoodCard key={item?.card?.info?.id} {...item?.card?.info}/>
-    )}
+            <FoodCard key={item?.card?.info?.id} {...item?.card?.info} total={totalPrice} />
+        )}
+        <p className="font-bold p-2 m-2 mr-20 flex justify-end">Total Price :<span> &#8377;</span>{totalPrice / 100}</p>
     </div>
 }
 export default Cart;
--- src/components/FoodCard.js
+++ src/components/FoodCard.js
@@ -3,11 +3,11 @@ import { IMG } from "../config";
 export const FoodCard=({
     name,
     price,
-    imageId
+    imageId,
 })=>{
     return <div className="flex justify-around items-center m-4">
-        <img style={{width:"80px"}} className="rounded " src={IMG+imageId} alt="img" />
-        <p>{name}</p>
+        <img style={{width:"100px",height:"60px"}} className="rounded " src={IMG+imageId} alt="img" />
+        <p style={{width:"200px"}}>{name}</p>
         <p>{price/100}</p>
     </div>
 }
