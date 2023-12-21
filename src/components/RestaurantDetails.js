06a4b4b77393d95621ab3c2ffef911b508cdeebb
Rachit <rachitshrm96@gmail.com>
2023-12-22 00:07:13 +0530

    updated RestaurantDetails

--- src/components/RestaurantDetails.js
+++ src/components/RestaurantDetails.js
@@ -22,11 +22,11 @@ const RestaurantDetails=()=>{
     :(
     <div className="MenuContainer">
         <div>
-            <h1> Restaurant Id : {id}</h1>
+            {/* <h1> Restaurant Id : {id}</h1> */}
             <h1 className="font-bold my-2 py-2">{restaurant.cards[0].card.card.info.name}</h1>
             <img className="rounded" style={{width:"280px",height:"150px",objectFit:"cover"}} src={IMG + restaurant.cards[0].card.card.info.cloudinaryImageId} alt="img" />
-            <p>{restaurant.cards[0].card.card.info.avgRating} 
-                <img style={{width:"14px"}} src={star} alt="star" /> 
+            <p className="flex item-center">{restaurant.cards[0].card.card.info.avgRating} 
+                <img className="ml-1 h-5 w-4 pt-1"  src={star} alt="star" /> 
             </p>
             <p style={{color:"grey",textOverflow:"ellipsis",overflow:"hidden",width:"200px",whiteSpace: "nowrap" }}>{restaurant.cards[0].card.card.info.cuisines.join(", ")}</p>
             <p>{restaurant.cards[0].card.card.info.locality}</p>
