f25f9f854d6cb381d05f2a100b50a0c7d1fc30cf
Rachit <rachitshrm96@gmail.com>
2023-12-21 22:58:47 +0530

    updated restaurantCard

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -13,7 +13,9 @@ export const RestaurantCard=({
     <div className='card'>
         <img style={{width:"280px",height:"150px",objectFit:"cover"}} src={IMG+cloudinaryImageId} alt="img" />
         <p style={{fontWeight:"bold"}}>{name}</p>
-        <p>{avgRating} <img style={{width:"14px"}} src={star} alt="star" /> </p>
+        <p className="flex items-center">
+            {avgRating} <img className="ml-1" style={{ width: "14px" }} src={star} alt="star" />
+        </p>
         <p style={{color:"grey",textOverflow:"ellipsis",overflow:"hidden",width:"200px",whiteSpace: "nowrap" }}>{cuisines.join(", ")}</p>
         <p>{locality}</p>
     </div>)
