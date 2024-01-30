868a7e1c728e8b96b0b58056e346e02402cc7ba2
Rachit <rachitshrm96@gmail.com>
2024-01-30 15:59:32 +0530

    footer logo blend

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ import logo from "../assets/TastyTracks.png"
 export default function Footer() {
   return (
     <div className='footer'>
-      <div><a href="/"><img className="logo-f" src={logo} alt="TastyTracks" /></a> </div>
-      <div style={{color:"white",padding:"5px"}}>2023@Rachit.Inc</div>
+      <div className='image-container'><a href="/"><img className="logo-f" src={logo} alt="TastyTracks" /></a> </div>
+      <div className='text-gray-500'>2023@Rachit.Inc</div>
     </div>
   )
 }
